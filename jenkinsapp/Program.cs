a16aab5693ad7547b7dadda4fd3e333357067b3b
SathwikaThummalapalli <sathwikathummalapalli428@gmail.com>
2022-07-15 15:48:47 +0530

    committ

--- jenkinsapp/Program.cs
+++ jenkinsapp/Program.cs
@@ -6,9 +6,16 @@ namespace jenkinsapp
     {
         static void Main(string[] args)
         {
+            MyMath m = new MyMath();
             Console.WriteLine("Hello World!");
             Console.WriteLine("welcome to jenkins");
             Console.ReadLine();
+            int a = m.add(10, 5);
+            int b = m.multiply(10, 5);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+            
         }
     }
 }
