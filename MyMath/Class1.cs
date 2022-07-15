43d5f6520272f9abc6142f78775069848886dce6
SathwikaThummalapalli <sathwikathummalapalli428@gmail.com>
2022-07-15 16:21:59 +0530

    committ

---
+++ MyMath/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Class1
+    {
+        int add(int a,int b)
+        {
+            return a + b;
+        }
+        int multiply(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
