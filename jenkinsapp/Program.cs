e49aa39d2b208cf0d45858e457d2401ebecea653
SathwikaThummalapalli <sathwikathummalapalli428@gmail.com>
2022-07-15 11:48:54 +0530

    Add project files.

---
+++ jenkinsapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace jenkinsapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to jenkins");
+            Console.ReadLine();
+        }
+    }
+}
