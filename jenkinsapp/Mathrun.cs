fa4284dcdccaa4a8eaed4dcf1652bfc50708c5f5
SathwikaThummalapalli <sathwikathummalapalli428@gmail.com>
2022-07-15 15:27:54 +0530

    committ

---
+++ jenkinsapp/Mathrun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jenkinsapp
+{
+    class Mathrun
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+            int a=math.add(5, 10);
+            int b=math.multiply(5, 10);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
